78374c223fe14df0d4b43c209193e9de2e22a941
Juan Jiménez <nefioxdev@gmail.com>
2021-09-24 00:27:52 -0500

    Lifecycle componentDidMount

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,41 @@
-import React from "react";
-import Card from './Card';
+// import render from "dom-serializer";
+import React, {Component} from "react";
+import Gif from './Gif';
 
-function Content() {
+class Content extends Component {
+    constructor() {
+        super();
+        this.state = {
+            gif: [],
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        console.log('GIF MOUNT');
+        fetch('https://api.giphy.com/v1/gifs/trending?api_key=YXWuXcT39KxyhvmHdXsYlOCKiQptAhJO')
+        .then(response => response.json())
+        .then(res => { this.setState({ gif: res.data }) })
+        .catch(err => console.log(err))
+        };
+
+    render() {
+        if (this.state.error) {
+            return <h1>Caught an error.</h1>
+          }
     return(
-        <div className="container">
-            <div className="row text-center">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </div>
-      </div>
+    <div className="container">
+        <div className="row text-center">
+            {
+                this.state.gif.map(gifs => {
+                    return <Gif {...gifs} key={gifs.id}/>
+                    // return <Gif url={gifs.images.original.url} title={gifs.title} key={gifs.id}/>
+                })
+            }
+        </div>
+    </div>
     );
+    }
 }
 
 export default Content;
